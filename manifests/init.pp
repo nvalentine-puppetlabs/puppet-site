b3d6484b5d1a376ba59e88e527700c77128643e2
Nathan Valentine <nathan@puppetlabs.com>
2013-05-09 10:44:23 -0700

    Change includes to requires to make sure that subclasses follow both parents classes and params classes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,7 +36,7 @@
 # Copyright 2011 Your name here, unless otherwise noted.
 #
 class site {
-  include site::params
+  require site::params
 
   notify { "site": }
 }
--- manifests/profile/params.pp
+++ manifests/profile/params.pp
@@ -1,5 +1,5 @@
 class site::profile::params {
-  include site::params
+  require site::params
   case $::osfamily {
     default: { notify { 'site::profile::params': } }
 #    default: {fail("OS family ${::osfamily} not supported by this module!")}
